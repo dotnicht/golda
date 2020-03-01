9419e1a3b658d9f2c2a4fa7b8c3d73a1234bef18
Nikolay Postoyko <p.nick@binebase.com>
2020-03-01 21:38:00 +0200

    clean common api

--- Common/Api/Controllers/ApiController.cs
+++ Common/Api/Controllers/ApiController.cs
@@ -1,16 +1,16 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
 
 namespace Binebase.Exchange.Common.Api.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
+    [ApiController, Route("api/[controller]/[action]")]
     public abstract class ApiController : ControllerBase
     {
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected IActionResult Convert(Unit _) => NoContent();
     }
 }
