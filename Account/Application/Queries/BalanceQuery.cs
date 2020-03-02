564295ee1540532d779608c8b0b540b38ff3fbe9
Nikolay Postoyko <dotnicht@live.com>
2020-03-02 15:40:34 +0200

    refactoring

--- Account/Application/Queries/BalanceQuery.cs
+++ Account/Application/Queries/BalanceQuery.cs
@@ -1,7 +1,11 @@
-﻿using Binebase.Exchange.AccountService.Domain.Common;
+﻿using Binebase.Exchange.AccountService.Domain.Aggregates;
+using Binebase.Exchange.AccountService.Domain.Common;
 using Binebase.Exchange.AccountService.Domain.Enums;
 using MediatR;
+using NEventStore.Domain.Persistence;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Binebase.Exchange.AccountService.Application.Queries
 {
@@ -9,5 +13,18 @@ namespace Binebase.Exchange.AccountService.Application.Queries
     {
         public Guid Id { get; set; }
         public Currency Currency { get; set; }
+
+        public class BalanceQueryHandler : IRequestHandler<BalanceQuery, BalanceQueryResult>
+        {
+            private readonly IRepository _repository;
+
+            public BalanceQueryHandler(IRepository repository) => _repository = repository;
+
+            public async Task<BalanceQueryResult> Handle(BalanceQuery request, CancellationToken cancellationToken)
+            {
+                var account = _repository.GetById<Account>(request.Id, int.MaxValue);
+                return await Task.FromResult(new BalanceQueryResult { Amount = account.Balance(request.Currency) });
+            }
+        }
     }
 }
