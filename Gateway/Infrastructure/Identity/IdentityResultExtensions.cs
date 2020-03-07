59bdcd0422cc340f943acea58dd64d82feff963a
Nikolay Postoyko <p.nick@binebase.com>
2020-03-07 20:58:59 +0200

    clean

--- Gateway/Infrastructure/Identity/IdentityResultExtensions.cs
+++ Gateway/Infrastructure/Identity/IdentityResultExtensions.cs
@@ -7,12 +7,6 @@ namespace Binebase.Exchange.Gateway.Infrastructure.Identity
 {
     public static class IdentityResultExtensions
     {
-        //private static readonly (Func<SignInResult, bool> Predicate, string Message)[] _mapping = new (Func<SignInResult, bool> Predicate, string Message)[]
-        //{
-        //    (x => x.IsLockedOut, "Account is locked out."),
-        //    (x => x.IsNotAllowed, "User not allowed to sign in."),
-        //};
-
         public static Result ToApplicationResult(this IdentityResult result)
             => result.Succeeded
                 ? Result.Success()
