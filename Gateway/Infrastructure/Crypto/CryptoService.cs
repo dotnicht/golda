e45a159d8d7c1613ecadb8d6e482a4d508173431
Nikolay Postoyko <p.nick@binebase.com>
2020-03-17 18:17:27 +0200

    fix

--- Gateway/Infrastructure/Crypto/CryptoService.cs
+++ Gateway/Infrastructure/Crypto/CryptoService.cs
@@ -22,29 +22,29 @@ namespace Binebase.Exchange.Gateway.Infrastructure.Crypto
             _configuration = options.Value;
             client.BaseAddress = _configuration.BaseUrl;
             _cryptoClient = new CryptoClient(_configuration.BaseUrl.ToString(), client);
-        } 
+        }
 
-        public async Task<string> GetAddress(Guid id, Currency currency)
+        public async Task<string> GetAddress(Guid id, Common.Domain.Currency currency)
         {
             return (await GetAddresses(id))[currency];
         }
 
-        public async Task<Dictionary<Currency, string>> GetAddresses(Guid id)
+        public async Task<Dictionary<Common.Domain.Currency, string>> GetAddresses(Guid id)
         {
             var result = await _cryptoClient.Addresses2Async(id);
-            return result.Addresses.ToDictionary(x => x.Currency, x => x.Public);
+            return result.Addresses.ToDictionary(x => (Common.Domain.Currency)x.Currency, x => x.Public);
         }
 
-        public async Task<string> GenerateAddress(Guid id, Currency currency)
+        public async Task<string> GenerateAddress(Guid id, Common.Domain.Currency currency)
         {
             var result = await _cryptoClient.AddressesAsync(new GenerateAddressCommand
             {
                 Id = id,
-                Currency = currency
-            }); 
-           return result.Address;
-        }
+                Currency = (Currency)currency
+            });
 
+            return result.Address;
+        }
 
         public class Configuration
         {
