e54506ee501706a13aa036559fe631c0b4447c8b
Nikolay Postoyko <p.nick@binebase.com>
2020-03-17 15:02:20 +0200

    rename

--- Crypto/Api/Controllers/CryptoController.cs
+++ Crypto/Api/Controllers/CryptoController.cs
@@ -10,7 +10,7 @@ namespace Binebase.Exchange.CryptoService.Api.Controllers
     public class CryptoController : ApiController
     {
         [HttpPost, ProducesResponseType(typeof(GenerateAddressCommandResult), StatusCodes.Status200OK)]
-        public async Task<ActionResult<GenerateAddressCommandResult>> Address(GenerateAddressCommand command)
+        public async Task<ActionResult<GenerateAddressCommandResult>> Addresses(GenerateAddressCommand command)
             => await Mediator.Send(command);
 
         [HttpGet, ProducesResponseType(typeof(AddressesQueryResult), StatusCodes.Status200OK)]
