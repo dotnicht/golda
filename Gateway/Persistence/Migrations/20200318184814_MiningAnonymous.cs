cb4abba816402d956d2689684e0e390510148ece
Nikolay Postoyko <p.nick@binebase.com>
2020-03-18 20:48:47 +0200

    migration

---
+++ Gateway/Persistence/Migrations/20200318184814_MiningAnonymous.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.Gateway.Persistence.Migrations
+{
+    public partial class MiningAnonymous : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAnomymous",
+                table: "MiningRequests");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnonymous",
+                table: "MiningRequests",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAnonymous",
+                table: "MiningRequests");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnomymous",
+                table: "MiningRequests",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
