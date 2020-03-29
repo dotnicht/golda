e378f7195e6176fb6b5ee447126434f3e85d3f9c
Nikolay Postoyko <p.nick@binebase.com>
2020-03-29 15:00:47 +0300

    migration

---
+++ Crypto/Infrastructure/Persistence/Migrations/20200329120025_BlockIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.CryptoService.Infrastructure.Persistence.Migrations
+{
+    public partial class BlockIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Block",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Block",
+                table: "Transactions",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
