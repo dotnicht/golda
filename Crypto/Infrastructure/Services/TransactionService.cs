bfc69eaf09e73cda8ce73214c7914745b415f389
Nikolay Postoyko <p.nick@binebase.com>
2020-03-30 12:42:54 +0300

    fix

--- Crypto/Infrastructure/Services/TransactionService.cs
+++ Crypto/Infrastructure/Services/TransactionService.cs
@@ -16,7 +16,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 using Transaction = Binebase.Exchange.CryptoService.Domain.Entities.Transaction;
