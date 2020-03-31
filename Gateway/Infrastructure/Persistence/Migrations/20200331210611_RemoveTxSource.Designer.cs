1d1f6fdd36aad13c621fce79487814a16fe41934
Nikolay Postoyko <p.nick@binebase.com>
2020-04-01 00:06:25 +0300

    migration

--- Gateway/Infrastructure/Persistence/Migrations/20200331210611_RemoveTxSource.Designer.cs
+++ Gateway/Infrastructure/Persistence/Migrations/20200331210611_RemoveTxSource.Designer.cs
@@ -4,14 +4,16 @@ using Binebase.Exchange.Gateway.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Binebase.Exchange.Gateway.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200331210611_RemoveTxSource")]
+    partial class RemoveTxSource
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gateway/Infrastructure/Persistence/Migrations/20200331210611_RemoveTxSource.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.Gateway.Infrastructure.Persistence.Migrations
+{
+    public partial class RemoveTxSource : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Transactions",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TokenAmount",
+                table: "Promotions",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrencyAmount",
+                table: "Promotions",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "MiningRequests",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "ExchangeOperations",
+                type: "decimal(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal (18,8)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Transactions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Transactions",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TokenAmount",
+                table: "Promotions",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrencyAmount",
+                table: "Promotions",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "MiningRequests",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "ExchangeOperations",
+                type: "decimal (18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,8)");
+        }
+    }
+}
