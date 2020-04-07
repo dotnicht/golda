aa5ef629b10571ebb93d95344165cbbb9149050a
Nikolay Postoyko <p.nick@binebase.com>
2020-04-07 18:26:10 +0300

    crypto exception

---
+++ Crypto/Domain/Exceptions/CryptoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Binebase.Exchange.CryptoService.Domain.Exceptions
+{
+
+    [Serializable]
+    public class CryptoException : Exception
+    {
+        public CryptoException() { }
+        public CryptoException(string message) : base(message) { }
+        public CryptoException(string message, Exception inner) : base(message, inner) { }
+        protected CryptoException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
