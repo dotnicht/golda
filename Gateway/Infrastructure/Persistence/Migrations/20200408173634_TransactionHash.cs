f26602673499ea126edb049ff6fec92a1592f909
Alexander Bondarchuk <alexander.bondarchuk@asposeptyltd.com>
2020-04-08 21:02:11 +0300

    impl

---
+++ Gateway/Infrastructure/Persistence/Migrations/20200408173634_TransactionHash.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.Gateway.Infrastructure.Persistence.Migrations
+{
+    public partial class TransactionHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "Transactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "Transactions");
+        }
+    }
+}
