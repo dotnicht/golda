aedd7654fd320061e5f73993bfbdff8d68207a92
Nikolay Postoyko <p.nick@binebase.com>
2020-04-15 12:36:00 +0300

    migration

---
+++ Crypto/Infrastructure/Persistence/Migrations/20200415093551_TxStatus.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.CryptoService.Infrastructure.Persistence.Migrations
+{
+    public partial class TxStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Transactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Transactions");
+        }
+    }
+}
