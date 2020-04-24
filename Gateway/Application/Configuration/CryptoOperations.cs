81c9c10ee3328e34c5c73f384edbc66b8ea087f8
Nikolay Postoyko <p.nick@binebase.com>
2020-04-24 16:35:14 +0300

    cfg

--- Gateway/Application/Configuration/CryptoOperations.cs
+++ Gateway/Application/Configuration/CryptoOperations.cs
@@ -5,6 +5,7 @@ namespace Binebase.Exchange.Gateway.Application.Configuration
     public class CryptoOperations : IConfig
     {
         public decimal WithdrawDailyLimit { get; set; }
+        public decimal WithdrawMinimum { get; set; }
         public bool WithdrawMultiRequired { get; set; }
         public int WithdrawMiningRequirement { get; set; }
         public int ExchangeMiningRequirement { get; set; }
