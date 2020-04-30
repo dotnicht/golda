2c5fc97978c13a406f9dd1a61378712dfde96042
Nikolay Postoyko <p.nick@binebase.com>
2020-04-30 12:26:46 +0300

    migration

---
+++ Gateway/Infrastructure/Persistence/Migrations/20200430092327_ExchangeAmounts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.Gateway.Infrastructure.Persistence.Migrations
+{
+    public partial class ExchangeAmounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "ExchangeOperations",
+                newName: "QuoteAmount");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BaseAmount",
+                table: "ExchangeOperations",
+                type: "decimal(18,8)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseAmount",
+                table: "ExchangeOperations");
+
+            migrationBuilder.RenameColumn(
+                name: "QuoteAmount",
+                table: "ExchangeOperations",
+                newName: "Amount");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Transactions",
+                type: "decimal(18,8)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
