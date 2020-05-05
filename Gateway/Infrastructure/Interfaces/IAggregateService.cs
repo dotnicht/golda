91cb4b418e54a9f88161cb4e5207397cf25c9a3c
Nikolay Postoyko <p.nick@binebase.com>
2020-05-05 12:19:23 +0300

    placeholders

---
+++ Gateway/Infrastructure/Interfaces/IAggregateService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binebase.Exchange.Gateway.Infrastructure.Interfaces
+{
+    public interface IAggregateService
+    {
+    }
+}
---
+++ Gateway/Infrastructure/Services/AggregateService.cs
@@ -0,0 +1,11 @@
+﻿using Binebase.Exchange.Gateway.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binebase.Exchange.Gateway.Infrastructure.Services
+{
+    public class AggregateService : IAggregateService
+    {
+    }
+}
