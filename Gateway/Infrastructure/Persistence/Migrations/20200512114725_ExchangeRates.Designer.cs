ef4af7aa8cfbf00fae00a873d8ad0edff7dec6d9
Nikolay Postoyko <p.nick@binebase.com>
2020-05-12 14:50:21 +0300

    migration

--- Gateway/Infrastructure/Persistence/Migrations/20200512114725_ExchangeRates.Designer.cs
+++ Gateway/Infrastructure/Persistence/Migrations/20200512114725_ExchangeRates.Designer.cs
@@ -4,14 +4,16 @@ using Binebase.Exchange.Gateway.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Binebase.Exchange.Gateway.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200512114725_ExchangeRates")]
+    partial class ExchangeRates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
