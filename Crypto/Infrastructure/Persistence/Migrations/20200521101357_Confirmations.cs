d4e98621ebc16bd8f6183493f1e35164b8f387a8
Nikolay Postoyko <p.nick@binebase.com>
2020-05-21 13:33:20 +0300

    migration

---
+++ Crypto/Infrastructure/Persistence/Migrations/20200521101357_Confirmations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Binebase.Exchange.CryptoService.Infrastructure.Persistence.Migrations
+{
+    public partial class Confirmations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Confimations",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Confimations",
+                table: "Transactions");
+        }
+    }
+}
