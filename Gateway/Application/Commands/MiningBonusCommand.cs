1a9d543d7cb0ea283ea6ef4e489f53370a540f02
Nikolay Postoyko <p.nick@binebase.com>
2020-06-01 11:23:00 +0300

    bonus mining created fix

--- Gateway/Application/Commands/MiningBonusCommand.cs
+++ Gateway/Application/Commands/MiningBonusCommand.cs
@@ -11,6 +11,7 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Binebase.Exchange.Gateway.Application.Commands
 {
@@ -39,12 +40,14 @@ namespace Binebase.Exchange.Gateway.Application.Commands
 
             public async Task<MiningBonusCommandResult> Handle(MiningBonusCommand request, CancellationToken cancellationToken)
             {
+                var created = _dateTime.UtcNow - _configuration.Weekly.Timeout;
+
                 var mining =_context.MiningRequests
                     .OrderByDescending(x => x.Created)
                     .FirstOrDefault(
                         x => (x.Type == MiningType.Weekly || x.Type == MiningType.Bonus || x.Type == MiningType.Default)
                         && (x.CreatedBy == _currentUserService.UserId || x.LastModifiedBy == _currentUserService.UserId)
-                        && _dateTime.UtcNow - x.Created < _configuration.Weekly.Timeout);
+                        && x.Created > created);
 
                 if (mining != null)
                 {
